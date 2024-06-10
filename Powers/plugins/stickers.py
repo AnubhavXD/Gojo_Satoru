6e0d31343d0f936c00b2bc57ad67b6e6616d9466
Anubhav <132827423+AnubhavXD@users.noreply.github.com>
2024-06-10 10:38:23 +0530

    Update stickers.py

--- Powers/plugins/stickers.py
+++ Powers/plugins/stickers.py
@@ -257,7 +257,7 @@ async def memify_it(c: Gojo, m: Message):
     kb = IKM(
         [
             [
-                IKB("Join for memes",url="https://t.me/memesofdank")
+                IKB("Join Network",url="https://t.me/Hogwarts_Network")
             ]
         ]
     )
@@ -353,8 +353,8 @@ __HELP__ = """
 • /stickerid (/stid) : Reply to any sticker to get it's id
 • /mmf <your text>: Reply to a normal sticker or a photo or video file to memify it. If you want to right text at bottom use `;right your message`
     ■ For e.g. 
-    ○ /mmf Hello freinds : this will add text to the top
-    ○ /mmf Hello ; freinds : this will add Hello to the top and freinds at the bottom
+    ○ /mmf Hello friends : this will add text to the top
+    ○ /mmf Hello ; friends : this will add Hello to the top and freinds at the bottom
     ○ /mmf ; Hello friends : this will add text at the bottom
     ○ /mmfb <text>: To fill text with black colour
     ○ /mmfw or /mmf <text>: To fill it with white colour
